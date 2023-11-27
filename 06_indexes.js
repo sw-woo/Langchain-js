bf236f2443afe81398753a19ca08e7fa8fa4db3d
sw-woo <swpheus1@gmail.com>
2023-11-27 15:44:39 +0900

    07_usestore.js 까지 업데이트

--- 06_indexes.js
+++ 06_indexes.js
@@ -2,19 +2,23 @@ import { config } from "dotenv";
 config();
 
 import { TextLoader } from "langchain/document_loaders/fs/text";
-import { CharacterTextSplitter } from "langchain/dist/text_splitter";
+
+//랭체인 characterTextSplitter 선언
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+
+//faissStore Facebook 메타에서 개발을 진행한 벡터 DB
 import { FaissStore } from "langchain/vectorstores/faiss";
 
 //step1. 데이터 가져오기 ./ 경로에 텍스트 파일 집어 넣기
 
-const loader = new TextLoader("./");
+const loader = new TextLoader("./restaurant.txt");
 
 const docs = await loader.load();
 
 //step2. 문자데이터 split chunkSize 자르는 크기,chunkOverlap 자르는 부분에 공통영역 크기
 
-const splitter = new CharacterTextSplitter({
+const splitter = new RecursiveCharacterTextSplitter({
 	chunkSize: 200,
 	chunkOverlap: 50,
 });
@@ -22,9 +26,9 @@ const splitter = new CharacterTextSplitter({
 const documents = await splitter.splitDocuments(docs);
 console.log(documents);
 
-//step3. 자른 문서데이터 임베딩
-
+//step3. 자른 문서데이터 임베딩 https://openai.com/blog/introducing-text-and-code-embeddings
 const embeddings = new OpenAIEmbeddings();
 
+//step4. 벡터DB FaissStore에 저장하기
 const vectorstore = await FaissStore.fromDocuments(documents, embeddings);
 await vectorstore.save("./");
--- 07_usestore.js
+++ 07_usestore.js
@@ -4,15 +4,19 @@ config();
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { FaissStore } from "langchain/vectorstores/faiss";
 import { OpenAI } from "langchain/llms/openai";
+//vector store DB 에서 찾기위한 RetrievalQAChain 과 RetrievalQAChain을 돕기 위한 loadQAStuffChain
 import { RetrievalQAChain, loadQAStuffChain } from "langchain/chains";
 
-//step1. 텍스트 임베딩 부르기
+//step1. 텍스트 임베딩 부르기 이유는 우리는 새로운 query(질의)를 유저로부터 벡터툴에 전달해서 similarity search 진행하기
 const embeddings = new OpenAIEmbeddings();
 //step2. 벡터 DB FaissStore 사용 선언
 const vectorstore = await FaissStore.load("./", embeddings);
 
 //step3. 모델 선언
-const model = new OpenAI({ temperature: 0.5 });
+const model = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY,
+	temperature: 0.7,
+});
 
 //step4. 검색 Q,A chain 객체 선언
 const chain = new RetrievalQAChain({
@@ -26,7 +30,7 @@ const chain = new RetrievalQAChain({
 
 // step5. RetrievalQAChain 비동기 실행 결고 res에 담기
 const res = await chain.call({
-	query: "when does the restaurant open on friday",
+	query: "음직점에서 아이들을 위한 키즈 메뉴가 있나요?",
 });
 
 // step6. 결과 부분 출력
